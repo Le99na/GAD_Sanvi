c747c6fae6af8b6cc606b2d3836a7f728c9e785a
Le99na <62903431+Le99na@users.noreply.github.com>
2024-12-04 14:06:26 -0500

    Update run_clusterCorr.m
    adjusted to Correlation test

--- run_clusterCorr.m
+++ run_clusterCorr.m
@@ -1,6 +1,6 @@
 clear all; clc
 
-[t_values2, p_values, sig_clusters] = cluster_permutation_test_oneSample(mat,10000, 0.05)
+[t_values2, p_values, sig_clusters] = cluster_permutation_test_Correlation2(mat,behav,10000, 0.05)
 
 
 
